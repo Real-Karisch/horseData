187ff3291e2afaa1af6e7c36b50623351d469a72
Jack Karisch <jack.karisch@gmail.com>
2020-08-16 19:33:24 -0500

    Updated

--- python/convertTxtToDFFns.py
+++ python/convertTxtToDFFns.py
@@ -49,21 +49,76 @@ def parseRace(raceChart):
         elif re.search('Off *at:', line) is not None:
             genInd = cnt + 2
         elif re.search('Last *Raced *Pgm', line) is not None:
-            horseInd = [cnt]
+            horseInd = [cnt + 1]
         elif re.search('Fractional *Times:', line):
             horseInd.append(cnt)
             endInd = cnt
         cnt += 1
-
+    """
     genItems = parseGenInfo(raceChart[:genInd], trackRecordInd=trackRecordInd)
 
     if genItems == 'Error':
         return 'Error - Quarter Horse'
-    #horseItems = parseHorseInfo(raceChart[horseInd[0]:horseInd[1]])
+    """
+    horseItems = parseHorseInfo(raceChart[horseInd[0]:horseInd[1]])
     #endItems = parseEndInfo(raceChart[endInd:])
 
-    genDF = pd.DataFrame(genItems, index=[0])
+    genDF = pd.DataFrame(horseItems, index=[0])
     
     return genDF
 
     #make df row, append to df, and return
+
+def parseHorseInfo(horseLines):
+    cnt = 0
+    horses = int(len(horseLines) / 2)
+
+    #horseDF = pd.DataFrame(columns=)
+
+    for i in [x*2 for x in range(horses)]:
+        itemsTop = parseHorseTop(horseLines[i])
+        itemsBottom = parseHorseBottom(horseLines[i+1])
+    
+    out = itemsTop + itemsBottom
+    return out
+
+def parseHorseTop(line):
+    fullSearch = re.search(r' *(\d?\d) *(\d?\d) *([A-Za-z0-9/]*) *([A-Za-z0-9/]*) *([A-Za-z0-9/]*) *([A-Za-z0-9/]*) *([A-Za-z0-9/]*) *', line)
+
+    lastRaceNum = fullSearch.group(1)
+    lastRacePlace = fullSearch.group(2)
+    lengthsSeg1 = fullSearch.group(3)
+    lengthsSeg2 = fullSearch.group(4)
+    lengthsSeg3 = fullSearch.group(5)
+    lengthsSeg4 = fullSearch.group(6)
+    lengthsFin = fullSearch.group(7)
+
+    out = [lastRaceNum, lastRacePlace, lengthsSeg1, lengthsSeg2, lengthsSeg3, lengthsSeg4, lengthsFin]
+    return out
+
+def parseHorseBottom(line):
+    fullSearch = re.search(r'''(\d?\d[A-Z][a-z]{2}\d\d) *([A-Z])* *(\d?\d) *([^0-9])(\d?\d?\d) *([A-Za-z])*
+     *(\d?\d) +(\d?\d) +(\d?\d) +(\d?\d) +(\d?\d) +(\d?\d) +(\d?\d) +([0-9]+\.\d\d)(.*)$''',
+     line)
+    
+    lastRaceDate = fullSearch.group(1)
+    lastRaceTrack = fullSearch.group(2)
+    program = fullSearch.group(3)
+    horseAndJockey = fullSearch.group(4)
+    weight = fullSearch.group(5)
+    m_e = fullSearch.group(6)
+    pp = fullSearch.group(7)
+    seg1 = fullSearch.group(8)
+    seg2 = fullSearch.group(9)
+    seg3 = fullSearch.group(10)
+    seg4 = fullSearch.group(11)
+    seg5 = fullSearch.group(12)
+    segFin = fullSearch.group(13)
+    odds = fullSearch.group(14)
+    comments = fullSearch.group(15)
+
+    out = [lastRaceDate,lastRaceTrack,program,horseAndJockey,weight,m_e,
+    seg1,seg2,seg3,seg4,seg5,segFin,odds,comments]
+
+
+    return out
